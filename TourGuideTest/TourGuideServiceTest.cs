fdd36ab9b99201a338d158ab04072b5ee0a44611
EveCrystali <evequeenrevenge@gmail.com>
2024-08-16 10:47:47 +0200

    fix(GetNearbyAttractions): return the 5 closest attractions ordered by proximity
    - update GetNearbyAttractions method to return the 5 closest attractions to the user, ordered by proximity
    - remove obsolete comments
    - remove unnecessary using statements
    - ensure the GetNearbyAttractions unit test passes
    
    Resolves issue: update GetNearbyAttraction to return the 5 closest attractions #8

--- TourGuideTest/TourGuideServiceTest.cs
+++ TourGuideTest/TourGuideServiceTest.cs
@@ -1,14 +1,6 @@
 ﻿using GpsUtil.Location;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TourGuide.Services;
 using TourGuide.Users;
-using TourGuide.Utilities;
 using TripPricer;
 
 namespace TourGuideTest
@@ -86,15 +78,13 @@ namespace TourGuideTest
             Assert.Equal(user.UserId, visitedLocation.UserId);
         }
 
-        // TODO: Modify this test. Its purpose is to make sure that the GetNearByAttractions method returns the 5 nearby attractions
         [Fact]
         public void GetNearbyAttractions()
         {
             // Arrange
             _fixture.Initialize(0);
             var user = new User(Guid.NewGuid(), "jon", "000", "jon@tourGuide.com");
-            // Add a test visited location to the user nearby 4 attractions
-            var visitedLocation = new VisitedLocation(user.UserId, new Locations(34.5, -116.5), DateTime.UtcNow);
+            var visitedLocation = _fixture.TourGuideService.TrackUserLocation(user);
             user.AddToVisitedLocations(visitedLocation);
 
             // Act
@@ -102,7 +92,7 @@ namespace TourGuideTest
             _fixture.TourGuideService.Tracker.StopTracking();
 
             // Assert
-            Assert.Equal(4, attractions.Count);
+            Assert.Equal(5, attractions.Count);
         }
 
         /// <summary>
