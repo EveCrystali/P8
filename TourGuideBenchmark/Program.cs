5c760b73768792510a6e92ffcff825fd73bf6fa7
RTX4070TI <evequeenrevenge@gmail.com>
2024-09-09 19:01:31 +0200

    chore(benchmark): configure sequential execution of GetTripDeals and GetUserLocation benchmarks -  update Program.cs to run benchmarks for GetTripDeals and GetUserLocation sequentially

--- TourGuideBenchmark/Program.cs
+++ TourGuideBenchmark/Program.cs
@@ -5,7 +5,7 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        // BenchmarkDotNet.Reports.Summary summary = BenchmarkRunner.Run<GetTripDealsBenchmark>();
-        BenchmarkDotNet.Reports.Summary summary = BenchmarkRunner.Run<GetUserLocationBenchmark>();
+        BenchmarkDotNet.Reports.Summary summary1 = BenchmarkRunner.Run<GetTripDealsBenchmark>();
+        BenchmarkDotNet.Reports.Summary summary2 = BenchmarkRunner.Run<GetUserLocationBenchmark>();
     }
 }
